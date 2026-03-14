f9cc8e189e871d6d4ef62058457eaffca10476b9
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2026-03-14 20:42:05 +0100

    test(translations): add tests for translation JSON files

---
+++ app/i18n/specs/common.translations.spec.ts
@@ -0,0 +1,14 @@
+import { crush } from "radashi";
+import { describe, it, expect } from "vitest";
+
+import frCommon from "~/i18n/locales/fr/common.json";
+import enCommon from "~/i18n/locales/en/common.json";
+
+describe("common.json translations", () => {
+  it("should have the same keys in english as in french when context is common.", () => {
+    const crushedFrCommonKeys = Object.keys(crush(frCommon)).toSorted();
+    const crushedEnCommonKeys = Object.keys(crush(enCommon)).toSorted();
+
+    expect(crushedEnCommonKeys).toStrictEqual<string[]>(crushedFrCommonKeys);
+  });
+});
---
+++ app/i18n/specs/question-themes.translations.spec.ts
@@ -0,0 +1,14 @@
+import { crush } from "radashi";
+import { describe, it, expect } from "vitest";
+
+import frQuestionThemes from "~/i18n/locales/fr/question-themes.json";
+import enQuestionThemes from "~/i18n/locales/en/question-themes.json";
+
+describe("question-themes.json translations", () => {
+  it("should have the same keys in english as in french when context is question-themes.", () => {
+    const crushedFrQuestionThemesKeys = Object.keys(crush(frQuestionThemes)).toSorted();
+    const crushedEnQuestionThemesKeys = Object.keys(crush(enQuestionThemes)).toSorted();
+
+    expect(crushedEnQuestionThemesKeys).toStrictEqual<string[]>(crushedFrQuestionThemesKeys);
+  });
+});
