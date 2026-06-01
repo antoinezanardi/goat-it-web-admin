133d75d42cd4b646680ec6d0c4dfea8d588d5f90
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2026-06-01 09:37:11 +0200

    feat(server): forward query params in question themes handler

--- tests/unit/utils/mocks/composables/nuxt/h3/h3.mock.ts
+++ tests/unit/utils/mocks/composables/nuxt/h3/h3.mock.ts
@@ -1,10 +1,20 @@
 import { vi } from "vitest";
 import type { Mock } from "vitest";
-import type { getRouterParam, readBody } from "h3";
+import type { getQuery, getRouterParam, readBody } from "h3";
+
+type GetQueryMock = Mock<typeof getQuery>;
 
 type GetRouterParameterMock = Mock<typeof getRouterParam>;
 
 type ReadBodyMock = Mock<typeof readBody>;
+
+/**
+ * Creates a mock implementation of the `getQuery` H3 global for unit testing purposes.
+ * Can only be used from unit tests setup functions.
+ */
+function createGetQueryMock(): GetQueryMock {
+  return vi.fn<typeof getQuery>().mockReturnValue({});
+}
 
 /**
  * Creates a mock implementation of the `getRouterParam` H3 global for unit testing purposes.
@@ -22,6 +32,6 @@ function createReadBodyMock(): ReadBodyMock {
   return vi.fn<typeof readBody>();
 }
 
-export type { GetRouterParameterMock as GetRouterParamMock, ReadBodyMock };
+export type { GetQueryMock, GetRouterParameterMock as GetRouterParamMock, ReadBodyMock };
 
-export { createGetRouterParameterMock as createGetRouterParamMock, createReadBodyMock };
+export { createGetQueryMock, createGetRouterParameterMock as createGetRouterParamMock, createReadBodyMock };
