b63808759111567a6a2e7aed2f9473f25cc6dce3
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2026-06-27 10:25:29 +0200

    feat(app): spa loader on boot

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -58,6 +58,7 @@ export default defineNuxtConfig({
       componentDetection: true,
     },
   },
+  spaLoadingTemplate: "templates/spa-loading-template.html",
   runtimeConfig: {
     goatItApi: {
       baseUrl: "",
