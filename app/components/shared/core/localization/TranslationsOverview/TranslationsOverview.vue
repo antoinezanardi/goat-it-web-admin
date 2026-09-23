83b611d26f30fd1dd1533aad160d06d33e66f9cf
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2026-09-23 17:14:11 +0200

    feat(localization): separate locale rows and list multi-value translations

--- app/components/shared/core/localization/TranslationsOverview/TranslationsOverview.vue
+++ app/components/shared/core/localization/TranslationsOverview/TranslationsOverview.vue
@@ -3,7 +3,7 @@ import { LOCALES } from "@goat-it/schemas/shared/locale";
 import type { Locale } from "@goat-it/schemas/shared/locale";
 
 import type { TranslationsOverviewProps } from "~/components/shared/core/localization/TranslationsOverview/translations-overview.types";
-import { getLocalizedDisplayValue, getLocalizedTextsDisplayValue, isLocalizedValueMissing } from "#shared/utils/helpers/localization/localization.helpers";
+import { getLocalizedDisplayValue, getLocalizedTextsDisplayValues, isLocalizedValueMissing } from "#shared/utils/helpers/localization/localization.helpers";
 
 const props = withDefaults(defineProps<TranslationsOverviewProps>(), {
   hideHeader: false,
@@ -12,16 +12,6 @@ const props = withDefaults(defineProps<TranslationsOverviewProps>(), {
 const { locale: currentLocale, t } = useI18n();
 
 const otherLocales = computed<Locale[]>(() => LOCALES.filter(locale => locale !== currentLocale.value));
-
-function getDisplayValue(locale: Locale): string {
-  if (props.localizedText) {
-    return getLocalizedDisplayValue(props.localizedText, locale) ?? t("localization.missingTranslation");
-  }
-  if (props.localizedTexts) {
-    return getLocalizedTextsDisplayValue(props.localizedTexts, locale) ?? t("localization.missingTranslation");
-  }
-  return t("localization.missingTranslation");
-}
 
 function isMissing(locale: Locale): boolean {
   if (props.localizedText) {
@@ -33,9 +23,31 @@ function isMissing(locale: Locale): boolean {
   return true;
 }
 
+function getDisplayValue(locale: Locale): string {
+  if (props.localizedText) {
+    return getLocalizedDisplayValue(props.localizedText, locale) ?? t("localization.missingTranslation");
+  }
+  return t("localization.missingTranslation");
+}
+
+function getDisplayValues(locale: Locale): string[] {
+  if (!props.localizedTexts) {
+    return [t("localization.missingTranslation")];
+  }
+  return getLocalizedTextsDisplayValues(props.localizedTexts, locale) ?? [t("localization.missingTranslation")];
+}
+
 function getDisplayValueClass(locale: Locale): string {
   return isMissing(locale) ? "text-error italic" : "text-default";
 }
+
+const localeDisplayData = computed(() => otherLocales.value.map(locale => ({
+  locale,
+  isList: Boolean(props.localizedTexts) && !isMissing(locale),
+  values: getDisplayValues(locale),
+  value: getDisplayValue(locale),
+  cssClass: getDisplayValueClass(locale),
+})));
 </script>
 
 <template>
@@ -53,22 +65,49 @@ function getDisplayValueClass(locale: Locale): string {
         {{ t("localization.otherTranslations") }}
       </div>
 
-      <USeparator class="mb-2"/>
+      <USeparator
+        class="mb-2"
+        data-testid="translations-overview-header-separator"
+      />
     </div>
 
     <div class="flex flex-col gap-1">
-      <div
-        v-for="locale in otherLocales"
-        :key="locale"
-        class="flex gap-2 items-center text-sm"
-        :data-testid="`locale-value-${locale}`"
+      <template
+        v-for="(data, index) in localeDisplayData"
+        :key="data.locale"
       >
-        <LocaleLabel :locale="locale"/>
+        <USeparator
+          v-if="index > 0"
+          :data-testid="`locale-separator-${data.locale}`"
+        />
 
-        <span :class="getDisplayValueClass(locale)">
-          {{ getDisplayValue(locale) }}
-        </span>
-      </div>
+        <div
+          class="flex gap-2 items-start text-sm"
+          :data-testid="`locale-value-${data.locale}`"
+        >
+          <LocaleLabel :locale="data.locale"/>
+
+          <ul
+            v-if="data.isList"
+            class="list-disc list-inside"
+          >
+            <li
+              v-for="(value, valueIndex) in data.values"
+              :key="valueIndex"
+              :class="data.cssClass"
+            >
+              {{ value }}
+            </li>
+          </ul>
+
+          <span
+            v-else
+            :class="data.cssClass"
+          >
+            {{ data.value }}
+          </span>
+        </div>
+      </template>
     </div>
   </div>
 </template>
